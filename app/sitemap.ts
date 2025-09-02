0c2abf8faf1a9fa285b661e1d4bd85b0a8e05c1c
aj-et <tumbokon.aaronjulius@outlook.com>
2025-09-01 22:01:14 -0600

    added robots and sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,25 @@
+import { MetadataRoute } from "next"
+import cities from "@/const/cities.json"
+
+export default function sitemap(): MetadataRoute.Sitemap {
+//   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"
+  const baseUrl = "https://www.jacksonholebatremoval.com/"
+
+  const staticPages: MetadataRoute.Sitemap = [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ]
+
+  const cityPages: MetadataRoute.Sitemap = cities.map((city) => ({
+    url: `${baseUrl}/${city.slug}`,
+    lastModified: new Date(),
+    changeFrequency: "weekly",
+    priority: 0.8,
+  }))
+
+  return [...staticPages, ...cityPages]
+}
